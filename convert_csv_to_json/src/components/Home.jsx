9ef509ce0a32a9fa6d265533a230b6d76be2e69f
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2022-03-04 12:36:56 +0100

    leaflet-api finished

---
+++ convert_csv_to_json/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Papa from 'papaparse';
+
+
+const Home = () => {
+    oilWorldProdUrl =
+        "https://raw.githubusercontent.com/JosepAnSabate/dades-oil-world-production/main/df_oil_year.csv"; 
+   
+    
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
--- leaflet_api/src/App.js
+++ leaflet_api/src/App.js
@@ -1,11 +1,29 @@
 import {useState, useEffect } from 'react'
 import Mapa from './components/Mapa';
+import Loading from './components/Loading';
+import Foot from './components/Foot';
 import './App.css';
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(function(){
+    const fetchEvents = async () => {
+      setLoading(true)
+      await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+              .then(res => res.json())
+              //.then(data => console.log(data.events))
+              .then(data => setEventData(data.events)) 
+    
+      setLoading(false) 
+    }
+    fetchEvents()
+  },[])
   return (
     <div>
-     <Mapa />
+     {!loading ?  <Mapa eventData={eventData} />:<Loading />}
+     <Foot />
     </div>
   );
 }
